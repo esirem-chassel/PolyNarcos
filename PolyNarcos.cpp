26e58ded135848a4435694eb548848fcc4e87faf
Quentin Chassel <quentin.chassel@u-bourgogne.fr>
2026-01-16 13:42:07 +0100

    Ajoutez des fichiers projet.

---
+++ PolyNarcos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Clinique gère des patients
+// Un examen (pouls, taux Co2, tension, état) est fait pour un patient sur une date donnée
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
